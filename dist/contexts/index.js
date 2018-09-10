db73ba3ac24416140ffd891e039d11a980670676
jarvispact <jarvispact@gmail.com>
2018-09-10 21:23:49 +0200

    fix build script section in package.json

---
+++ dist/contexts/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _ThemeContext = require('./ThemeContext');
+
+Object.defineProperty(exports, 'ThemeContext', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_ThemeContext).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
---
+++ dist/utils/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Theme = require('./Theme');
+
+Object.defineProperty(exports, 'Theme', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_Theme).default;
+  }
+});
+
+var _ThemeChanger = require('./ThemeChanger');
+
+Object.defineProperty(exports, 'ThemeChanger', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_ThemeChanger).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
