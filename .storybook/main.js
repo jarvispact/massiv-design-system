c6f2eb42fcbe0290be11095e64de3924633d9bbd
jarvispact <jarvispact@gmail.com>
2020-06-25 21:52:24 +0200

    update deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,16 @@
 module.exports = {
-    stories: ["../src/**/*.stories.tsx"],
+    stories: ['../docs/**/*.stories.tsx'],
     addons: ['@storybook/addon-actions'],
     webpackFinal: async (config) => {
         config.module.rules.push({
             test: /\.(ts|tsx)$/,
-            loader: require.resolve("babel-loader"),
+            loader: require.resolve('babel-loader'),
             options: {
-                presets: [["react-app", { flow: false, typescript: true }]]
-            }
+                presets: [['react-app', { flow: false, typescript: true }]],
+            },
         });
 
-        config.resolve.extensions.push(".ts", ".tsx");
+        config.resolve.extensions.push('.ts', '.tsx');
         return config;
-    }
+    },
 };
